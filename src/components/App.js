37a863d9ad4f13298293721c3514b95b096d6561
kambleps <125243027+kambleps@users.noreply.github.com>
2023-07-19 06:33:49 +0000

    two

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,43 @@ import './../styles/App.css';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('https://dummyjson.com/products');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
         const jsonData = await response.json();
         setData(jsonData);
+        setIsLoading(false);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        setError('Error fetching data');
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!data) {
+    return <div>No data found</div>;
+  }
+
   return (
     <div>
-      <h1>Loading......</h1>
+      <h1>Data from API:</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
